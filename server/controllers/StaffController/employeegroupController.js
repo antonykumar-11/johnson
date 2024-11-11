940e5c6a9fc0f4caf4f289bd248e950487015d2b
antonykumar-11 <wellnessveda7@gmail.com>
2024-11-11 18:10:24 +0530

    Update employeegroupController

--- server/controllers/StaffController/employeegroupController.js
+++ server/controllers/StaffController/employeegroupController.js
@@ -9,10 +9,11 @@ exports.createEmployeeGroup = async (req, res) => {
 
     // Set the base URL dynamically
     let avatar;
-    const BASE_URL =
-      process.env.BACKEND_URL || `${req.protocol}://${req.get("host")}`;
+    let BASE_URL = process.env.BACKEND_URL;
+    if (process.env.NODE_ENV === "production") {
+      BASE_URL = `${req.protocol}://${req.get("host")}`;
+    }
 
-    // If there's a file, set the avatar URL
     if (req.file) {
       avatar = `${BASE_URL}/uploads/user/${req.file.originalname}`;
     }
@@ -70,14 +71,14 @@ exports.updateEmployeeGroup = async (req, res) => {
     console.log("coming namereq.body", req.body);
     // Set the base URL dynamically
     let avatar;
-    const BASE_URL =
-      process.env.BACKEND_URL || `${req.protocol}://${req.get("host")}`;
+    let BASE_URL = process.env.BACKEND_URL;
+    if (process.env.NODE_ENV === "production") {
+      BASE_URL = `${req.protocol}://${req.get("host")}`;
+    }
 
-    // If there's a file, set the new avatar URL
     if (req.file) {
       avatar = `${BASE_URL}/uploads/user/${req.file.originalname}`;
     }
-
     const employee = await Employee.findOneAndUpdate(
       { _id: req.params.id, owner: req.user.id }, // Ensure the user owns the employee record
       { name, date, avatar }, // Include avatar update if present
